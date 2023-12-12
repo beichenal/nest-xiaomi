09a7896e8892018d534502acfdd504bd9710fe47
dengliangdong <1615527807@qq.com>
2023-12-12 23:12:55 +0800

    save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './module/admin/admin.module';
+import { ApiModule } from './module/api/api.module';
+import { DefaultModule } from './module/default/default.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, ApiModule, DefaultModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/module/admin/main/main.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('admin')
+export class MainController {
+  @Get()
+  index() {
+    return '我是后台首页';
+  }
+}
