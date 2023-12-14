30cb3e09cd3cc101742e86601e8c1e970c902b28
dengliangdong <1615527807@qq.com>
2023-12-14 22:46:54 +0800

    views

--- public/admin/js/base.js
+++ public/admin/js/base.js
@@ -1,35 +1,27 @@
-$(function(){
-	
-	app.init();
+$(function () {
+  app.init();
+});
 
-})
+var app = {
+  init: function () {
+    this.slideToggle();
+    this.resizeIframe();
+  },
+  resizeIframe: function () {
+    //1、获取浏览器的高度
+    //2、设置iframe的高度
 
-var app={
+    // alert($(window).height());
 
-	init:function(){
-		this.slideToggle();
-		this.resizeIframe();
+    $('#rightMain').height($(window).height() - 80);
+  },
+  slideToggle: function () {
+    $('.aside h4').click(function () {
+      $(this).siblings('ul').slideToggle();
+    });
+  },
+};
 
-	},
-	resizeIframe:function(){
-		//1、获取浏览器的高度
-		//2、设置iframe的高度
-
-		// alert($(window).height());
-
-		$("#rightMain").height($(window).height()-80);
-	},
-	slideToggle:function(){
-		$('.aside h4').click(function(){		
-						
-					
-					$(this).siblings('ul').slideToggle();
-		})
-	}
-
-}
-
-window.onresize=function(){
-
-	app.resizeIframe();
-}
+window.onresize = function () {
+  app.resizeIframe();
+};
