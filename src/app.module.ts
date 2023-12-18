a70f9379668a036cf72f2acd868cd52b87ed9899
dengliangdong <1615527807@qq.com>
2023-12-18 21:59:24 +0800

    auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './module/admin/admin.module';
 import { ApiModule } from './module/api/api.module';
 import { DefaultModule } from './module/default/default.module';
 import { ToolsService } from './service/tools/tools.service';
+import { AdminauthMiddleware } from './middleware/adminauth/adminauth.middleware';
 
 @Module({
   imports: [AdminModule, ApiModule, DefaultModule],
-  controllers: [AppController],
-  providers: [AppService, ToolsService],
+  controllers: [],
+  providers: [ToolsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AdminauthMiddleware).forRoutes('admin/*');
+  }
+}
---
+++ src/middleware/adminauth/adminauth.middleware.ts
@@ -0,0 +1,27 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class AdminauthMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    console.log('admin auth 中间件');
+    // 获取访问录取
+    const path = req.baseUrl;
+    console.log('path', path);
+    const whiteList = [
+      '/admin/login',
+      '/admin/login/code',
+      '/admin/login/doLogin',
+    ];
+    if (whiteList.includes(path)) {
+      next();
+    } else {
+      // 获取session 保存的用户信息
+      const userinfo = req.session.userinfo;
+      if (userinfo && userinfo.username) {
+        next();
+      } else {
+        res.redirect('/admin/login');
+      }
+    }
+  }
+}
